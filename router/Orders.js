60c38cb609ee4e5731d4d8cd1a78627de462584a
Cyril <cyrilesin214@email.com>
2026-08-19 07:41:45 +0100

    commit

--- router/Orders.js
+++ router/Orders.js
@@ -34,13 +34,31 @@ router.get("/Orders", async (req, res) => {
                     take: 1
                 }
             },
-
-
         })
 
         res.json({ Message: "Order", Orders })
+
+    } catch (error) {
+
+        console.log(error)
+
+    }
+})
+
+router.get("/Order/:id", async (req, res) => {
+    try {
+        const Order = await prisma.orders.findFirst({
+            where: { orderid: req.params.id },
+            include: {
+                Address: true,
+                OderItem: true
+            }
+        })
+
+        res.status(200).json({ Message: "Order Details", Order })
     } catch (error) {
         console.log(error)
+        res.status(500).json({ Message: "Internal Error" })
     }
 })
 
