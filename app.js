ba4d78792974d0b5d596f1d3b85e25e1394d8519
Cyril <cyrilesin214@gmail.com>
2026-09-01 19:06:24 +0100

    delpoyment

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import "./Oauth2/PassportConfig.js";
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://zyloo-zeta.vercel.app/",
         credentials: true
     })
 )
