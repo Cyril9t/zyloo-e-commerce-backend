faec84ea55689750bb0fdbd948dd5e28e88a3154
Cyril <cyrilesin214@gmail.com>
2026-09-05 05:38:03 +0100

    payment-config for production

--- router/CheckOut.js
+++ router/CheckOut.js
@@ -3,135 +3,6 @@ import prisma from "../prismaConfig/prisma.js";
 import https from "https"
 const router = express.Router()
 
-// router.post("/checkOuts", async (req, res) => {
-//     try {
-//         const user = req.user
-
-//         const { item, name, color, image, size, quantity, price, total, email, firstName, lastName, StreetAddress, city, state, postalCode, paymentMethod } = req.body
-
-//         if (
-//             !total ||
-//             !email ||
-//             !firstName ||
-//             !lastName ||
-//             !StreetAddress ||
-//             !city ||
-//             !state ||
-//             !postalCode ||
-//             !paymentMethod
-//         ) {
-//             return res.status(400).json({
-//                 Message: "All fields are required"
-//             })
-//         }
-
-//         const code = Math.floor(1000000 + Math.random() * 900000);
-
-//         console.log(code)
-
-//         const Order = await prisma.orders.create({
-//             data: {
-//                 orderid: `ZYL-${code}`, total, user: {
-//                     connect: {
-//                         id: user.id
-//                     }
-//                 }
-
-//             }
-//         })
-
-//         const Address = await prisma.checkoutAddress.create({
-//             data: {
-//                 email, firstName, lastName, StreetAddress, city, state, postalCode, paymentMethod, OrderItem: { connect: { id: Order.id } }
-//             }
-//         })
-
-
-//         const orderId = Order.id
-
-//         const OrderItem = await prisma.orderItems.createMany({
-//             data: item.map((i) => ({
-//                 name: i.productItem.product.name,
-//                 color: i.productItem.color,
-//                 image: i.productItem.image,
-//                 size: i.productItem.size,
-//                 price: i.productItem.price,
-//                 quantity: i.quantity,
-//                 orderId
-//             }))
-//         })
-
-
-//         const OrderPlaced = await prisma.orders.findUnique({
-//             where: { id: Order.id },
-//             include: {
-//                 OderItem: true,
-//                 Address: true
-//             }
-//         })
-
-
-//         const params = JSON.stringify({
-//             "email": email,
-//             "amount": OrderPlaced.total * 100
-//         })
-
-//         const options = {
-//             hostname: 'api.paystack.co',
-//             port: 443,
-//             path: '/transaction/initialize',
-//             method: 'POST',
-//             headers: {
-//                 Authorization: `Bearer ${process.env.PAYSTACK_TEST_KEY}`,
-//                 'Content-Type': 'application/json'
-//             }
-//         }
-
-//         const paystackReq = https.request(options, paySatck => {
-//             let data = ''
-
-//             paySatck.on('data', (chunk) => {
-//                 data += chunk
-//             });
-
-//             paySatck.on('end', () => {
-//                 const response = JSON.parse(data)
-//                 console.log(response)
-//                 res.status(200).json({ Message: "Order success", OrderPlaced: OrderPlaced, response })
-//             })
-//         }).on('error', error => {
-//             console.error(error)
-//         })
-
-//         paystackReq.write(params)
-//         paystackReq.end()
-
-//         // const findCart = await prisma.cart.findUnique({
-//         //     where: { userId: user.id }
-//         // })
-
-//         // if (!findCart) return res.json({ Message: "No cart found" })
-
-//         // const deleteItem = await prisma.cartItem.deleteMany({
-//         //     where: { cartId: findCart.id }
-//         // })
-
-//         // const deleteCart = await prisma.cart.delete({
-//         //     where: {
-//         //         id: findCart.id
-//         //     }
-//         // })
-
-
-//         // res.json({ Message: "Order success", OrderPlaced: OrderPlaced })
-
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({ Message: " internal Error" })
-//     }
-// })
-
-
 router.post("/checkOuts", async (req, res) => {
     try {
         const user = req.user;
@@ -212,7 +83,7 @@ router.post("/checkOuts", async (req, res) => {
             email,
             amount,
             callback_url:
-                "http://localhost:5173/verifyPayment"
+                "https://zyloo-five.vercel.app/verifyPayment"
         });
 
         const options = {
